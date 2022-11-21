8ff43a68b31ef7983bf8f4f0ab25e19dc9edef29
Natalie <79932225+Nadia982@users.noreply.github.com>
2022-11-21 14:10:13 +0000

    draft 1 of project

---
+++ src/components/CountryInfo/CountryInfo.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { apiURL } from "../util/api";
+import { Link } from "react-router-dom";
+
+const CountryInfo = () => {
+  const [country, setCountry] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const { countryName } = useParams();
+
+  const getCountryByName = async () => {
+    try {
+      const res = await fetch(`${apiURL}/name/${countryName}`);
+
+      if (!res.ok) throw new Error("Could not find!");
+
+      const data = await res.json();
+
+      setCountry(data);
+
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getCountryByName();
+  }, [countryName]);
+
+  return (
+    <div className="country_info_wrapper">
+      <button>
+        <Link to="/">Back</Link>
+      </button>
+
+      {country?.map((country, index) => (
+        <div className="country_info_container" key={index}>
+          <div className="country_info-img">
+            <img src={country.flags.png} alt={country.name} />
+          </div>
+
+          <div className="country_info">
+            <div className="country_info-left">
+              <h3 id="country_info_title">{country.name.common}</h3>
+              <h5>
+                Population:{" "}
+                <span>
+                  {new Intl.NumberFormat().format(country.population)}
+                </span>
+              </h5>
+              <h5>
+                Region: <span>{country.region}</span>
+              </h5>
+              <h5>
+                Sub-region: <span>{country.subregion}</span>
+              </h5>
+              <h5>
+                Capital: <span>{country.capital}</span>
+              </h5>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default CountryInfo;
---
+++ src/components/Search/SearchInput.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const SearchInput = ({ onSearch }) => {
+  const [input, setInput] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    onSearch(input);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <input
+        type="text"
+        placeholder="Search for a country......"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        name=""
+        id=""
+      />
+    </form>
+  );
+};
+
+export default SearchInput;
